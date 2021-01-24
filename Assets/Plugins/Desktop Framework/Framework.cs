90def4395b907ca99ad300b54972319773df2061
mcpgnz <daniel.wojnarowski@moonlit.games>
2021-01-24 02:35:40 +0100

    feat(Framework): you'd be amazed what the new desklop-lib.dll by DesktopGames(TM) can do.

--- Assets/Plugins/Desktop Framework/Framework.cs
+++ Assets/Plugins/Desktop Framework/Framework.cs
@@ -10,7 +10,8 @@
     [StructLayout(LayoutKind.Sequential)]
     public struct item_data
     {
-        public IntPtr name;
+        public IntPtr name;  /* string ptr */
+        public int index;
         public int x;
         public int y;
     }
@@ -23,7 +24,7 @@
         public static void Initialize()
         {
             Initialize(OnUnmanagedInfo, OnUnmanagedError);
-            DesktopEx.Initialize();
+            DesktopEx.desktop_initialize();
         }
         #endregion Api
 
@@ -50,7 +51,7 @@
     }
 
     [Serializable]
-    public static class DesktopEx
+    public static unsafe class DesktopEx
     {
         #region Api
         public static ItemEx[] Items
@@ -58,53 +59,72 @@
             get
             {
                 /* fetch data */
-                var count = GetFoldersCount();
-                var data = new item_data[count];
-                GetFolders(data);
+                var count = desktop_folders_count();
+                _Items = (item_data*)Marshal.AllocHGlobal(Marshal.SizeOf(typeof(item_data)) * count);
+                desktop_folders(_Items);
 
                 /* create representation */
                 var items = new ItemEx[count];
                 for (int i = 0; i < count; ++i)
                 {
-                    items[i] = new ItemEx(data[i]);
+                    var item = (item_data*)IntPtr.Add((IntPtr)_Items, i * sizeof(item_data));
+                    items[i] = new ItemEx(item);
                 }
                 return items;
             }
         }
         #endregion Api
 
+        #region Private Variables
+        internal static item_data* _Items;
+        #endregion Private Variables
+
         #region Import
-        [DllImport("desktop-lib.dll", EntryPoint = "desktop_initialize")]
-        internal static extern void Initialize();
+        [DllImport("desktop-lib.dll")]
+        internal static extern void desktop_initialize();
 
-        [DllImport("desktop-lib.dll", EntryPoint = "desktop_folders_count")]
-        private static extern int GetFoldersCount();
+        [DllImport("desktop-lib.dll")]
+        private static extern int desktop_folders_count();
 
-        [DllImport("desktop-lib.dll", EntryPoint = "desktop_folders", CallingConvention = CallingConvention.Cdecl)]
-        private static extern void GetFolders([MarshalAs(UnmanagedType.LPArray)] item_data[] folders);
+        [DllImport("desktop-lib.dll", CallingConvention = CallingConvention.Cdecl)]
+        private static extern void desktop_folders([MarshalAs(UnmanagedType.LPArray)] item_data* folders);
         #endregion Import
     }
 
     [Serializable]
-    public sealed class ItemEx
+    public sealed unsafe class ItemEx
     {
         #region Public Variables
         [ShowInInspector]
-        public string Name => Marshal.PtrToStringUni(_Data.name);
+        public string Name => Marshal.PtrToStringUni(_Data->name);
 
         [ShowInInspector]
-        public Vector2Int Position => new Vector2Int(_Data.x, _Data.y);
+        public Vector2Int Position
+        {
+            get => new Vector2Int(_Data->x, _Data->y);
+            set
+            {
+                _Data->x = value.x;
+                _Data->y = value.y;
+                item_update_position(_Data);
+            }
+        }
         #endregion Public Variables
 
         #region Public Methods
-        public ItemEx(item_data data)
+        public ItemEx(item_data* data)
         {
             _Data = data;
         }
         #endregion Public Methods
 
+        #region Import
+        [DllImport("desktop-lib.dll")]
+        private static extern void item_update_position([MarshalAs(UnmanagedType.LPArray)] item_data* token);
+        #endregion Import
+
         #region Private Variables
-        private item_data _Data;
+        private item_data* _Data;
         #endregion Private Variables
     }
 }
