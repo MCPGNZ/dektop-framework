48b3576966fe44ccdb692c85658a3c19f155fa72
dextero <hukutizuviki@gmail.com>
2021-01-28 22:08:19 +0000

    Add SetWallpaperDemo

---
+++ Assets/_Scripts/Framework/WallpaperManager.cs
@@ -0,0 +1,49 @@
+﻿namespace Mcpgnz.DesktopFramework.Framework
+{
+    using Microsoft.Win32;
+    using System.Runtime.InteropServices;
+
+    // https://code.4noobz.net/set-the-wallpaper-by-code/
+    public static class WallpaperManager
+    {
+        const int SPI_SETDESKWALLPAPER = 20;
+        const int SPIF_UPDATEINIFILE = 0x01;
+        const int SPIF_SENDWININICHANGE = 0x02;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+
+        public enum Style : int
+        {
+            Tiled,
+            Centered,
+            Stretched
+        }
+
+        public static void Set(string imgPath, Style style)
+        {
+            var key = Registry.CurrentUser.OpenSubKey(@"Control Panel\Desktop", true);
+
+            switch (style)
+            {
+                case Style.Tiled:
+                    key.SetValue(@"WallpaperStyle", 1.ToString());
+                    key.SetValue(@"TileWallpaper", 1.ToString());
+                    break;
+                case Style.Centered:
+                    key.SetValue(@"WallpaperStyle", 1.ToString());
+                    key.SetValue(@"TileWallpaper", 0.ToString());
+                    break;
+                case Style.Stretched:
+                    key.SetValue(@"WallpaperStyle", 2.ToString());
+                    key.SetValue(@"TileWallpaper", 0.ToString());
+                    break;
+                default:
+                    break;
+            }
+
+            SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, imgPath, SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+        }
+    }
+
+}
