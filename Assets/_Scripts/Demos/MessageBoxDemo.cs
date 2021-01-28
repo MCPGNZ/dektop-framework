5d0435a4e98a0cf8d110fb68aa364d561956e0da
dextero <hukutizuviki@gmail.com>
2021-01-28 21:06:14 +0000

    Add MessageBoxDemo

---
+++ Assets/_Scripts/Demos/MessageBoxDemo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using UnityEngine;
+using UnityRawInput;
+
+public class MessageBoxDemo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        RawMouseInput.Start(workInBackground: true);
+        RawMouseInput.OnMouseRightDown += SpawnMessageBox;
+    }
+
+    void SpawnMessageBox(MousePosition pos)
+    {
+        switch (MessageBox.Show("tak czy nie?", "kapszon", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Asterisk))
+        {
+            case DialogResult.Yes:
+            case DialogResult.No:
+            case DialogResult.Cancel:
+                break;
+        }
+    }
+}
