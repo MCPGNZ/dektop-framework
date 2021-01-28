5dda23642698b3c1f2c89ec90c0031b1cd471848
mcpgnz <daniel.wojnarowski@moonlit.games>
2021-01-28 22:51:08 +0100

    feat(Game): added store/restore support for desktop parameters

--- Assets/_Scripts/Gameplay/Game.cs
+++ Assets/_Scripts/Gameplay/Game.cs
@@ -9,13 +9,42 @@
         public void Awake()
         {
             FrameworkEx.Initialize();
-            RawKeyInput.Start(true);
+
+            StoreSettings();
+            Setup();
         }
         private void OnDestroy()
         {
             RawKeyInput.Stop();
             FrameworkEx.Cleanup();
+
+            RestoreSettings();
         }
         #endregion Unity Methods
+
+        #region Private Variables
+        private bool _AutoArrange;
+        private bool _GridAlign;
+        #endregion Private Variables
+
+        #region Private Methods
+        private void Setup()
+        {
+            RawKeyInput.Start(true);
+            DesktopEx.AutoArrange = false;
+            DesktopEx.GridAlign = false;
+        }
+
+        private void StoreSettings()
+        {
+            _AutoArrange = DesktopEx.AutoArrange;
+            _GridAlign = DesktopEx.GridAlign;
+        }
+        private void RestoreSettings()
+        {
+            DesktopEx.AutoArrange = _AutoArrange;
+            DesktopEx.GridAlign = _GridAlign;
+        }
+        #endregion Private Methods
     }
 }
