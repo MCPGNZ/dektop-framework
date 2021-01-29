280afda79bbf79b54bf981377df1d2a8661719cc
Szymon Maj <fishbaitharry@gmail.com>
2021-01-29 13:58:22 +0100

    level changing works but only right side

---
+++ Assets/_Scripts/Gameplay/NextAreaTrigger.cs
@@ -0,0 +1,34 @@
+﻿
+namespace Mcpgnz.DesktopFramework
+{
+    using UnityEngine;
+
+    public class NextAreaTrigger : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.gameObject.name != "explorer") { Debug.Log("Should not happen lol."); }
+            if (_LeftWall) { _OverworldArea.GoLeft(); }
+            if (_RightWall) { _OverworldArea.GoRight(); }
+        }
+
+        [SerializeField] private OverworldArea _OverworldArea;
+        [SerializeField] private bool _LeftWall = false;
+        [SerializeField] private bool _RightWall = false;
+        [SerializeField] private bool _TopWall = false;
+        [SerializeField] private bool _BottomWall = false;
+    }
+}
