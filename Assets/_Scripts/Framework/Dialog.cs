29ba845a78a7675169abc64a4d7344d6325eec84
mcpgnz <daniel.wojnarowski@moonlit.games>
2021-01-30 00:41:19 +0100

    story beggins

---
+++ Assets/_Scripts/Framework/Dialog.cs
@@ -0,0 +1,26 @@
+﻿namespace Mcpgnz.DesktopFramework
+{
+    public static class Dialog
+    {
+        public static string Clipper(string message, params string[] options)
+        {
+            return Message("Clipper", message, null, options);
+        }
+
+        public static string Message(string title, string message, IconEx icon, params string[] options)
+        {
+            var iconPath = icon == null ? null : icon._Path;
+
+            string response = null;
+            var msg = new MessageBox(title, message, options, iconPath)
+            {
+                OnButtonClick = r =>
+                {
+                    response = r;
+                }
+            };
+            msg.ShowDialog();
+            return response;
+        }
+    }
+}
--- Assets/_Scripts/Gameplay/Story.cs
+++ Assets/_Scripts/Gameplay/Story.cs
@@ -12,11 +12,8 @@
         #region Unity Methods
         private void Start()
         {
-            /* setup explorer starting position */
-            _Explorer.transform.position = _Parser.Explorer_UnityPosition;
-
-            /* load first level */
-            _Overworld.Load(_Begin);
+            Begin();
+            BeginAction();
         }
         #endregion Unity Methods
 
@@ -25,5 +22,38 @@
         [Inject] private Explorer _Explorer;
         [Inject] private LevelParser _Parser;
         #endregion Private Variables
+
+        #region Private Methods
+        private void Begin()
+        {
+
+        START:
+            var response = Dialog.Clipper("No!", "Yes", "No");
+            switch (response)
+            {
+                case "Yes":
+                {
+                    goto START;
+                }
+                case "No":
+                {
+                    Dialog.Clipper("I am FED UP", "???");
+                    Dialog.Clipper("No more playing games!", ":<");
+                    break;
+                }
+            }
+
+        }
+
+        private void BeginAction()
+        {
+            /* setup explorer starting position */
+            _Explorer.transform.position = _Parser.Explorer_UnityPosition;
+
+            /* load first level */
+            _Overworld.Load(_Begin);
+        }
+        #endregion Private Methods
+
     }
 }
