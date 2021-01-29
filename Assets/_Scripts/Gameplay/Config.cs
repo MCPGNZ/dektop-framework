85bef368248734d7e0abbd1489a0e1e7f7f84621
mcpgnz <daniel.wojnarowski@moonlit.games>
2021-01-29 23:18:13 +0100

    added unity size to config

--- Assets/_Scripts/Gameplay/Config.cs
+++ Assets/_Scripts/Gameplay/Config.cs
@@ -8,6 +8,7 @@
     public sealed class Config : ScriptableObjectSingleton<Config>
     {
         #region Public Variables
+        public static Vector2Int UnitySize => Instance._UnitySize;
         public static Vector2Int StageSize => Instance._StageSize;
 
         public static float MovementSpeed => Instance._MovementSpeed;
@@ -15,6 +16,9 @@
         #endregion Public Variables
 
         #region Private Variables
+        [SerializeField, BoxGroup("Stages")]
+        private Vector2Int _UnitySize = new Vector2Int(1920, 1080);
+
         [SerializeField, BoxGroup("Stages")]
         private Vector2Int _StageSize = new Vector2Int(21, 10);
 
