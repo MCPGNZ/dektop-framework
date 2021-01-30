55c5e54f85c611d188869510c4bfc9fa1dcf8d1f
mcpgnz <daniel.wojnarowski@moonlit.games>
2021-01-30 01:39:12 +0100

    feat(HUD): added installer:

---
+++ Assets/_Scripts/Gameplay/Installers/HUDInstaller.cs
@@ -0,0 +1,19 @@
+﻿namespace Mcpgnz.DesktopFramework
+{
+    using Mcpgnz.Utilities;
+    using UnityEngine;
+
+    public sealed class HUDInstaller : PreviewInstaller<HUDController>
+    {
+        #region Public Methods
+        public override void InstallBindings()
+        {
+            Container.BindInstance(_Controller).AsSingle().NonLazy();
+        }
+        #endregion Public Methods
+
+        #region Inspector Variables
+        [SerializeField] private HUDController _Controller;
+        #endregion Inspector Variables
+    }
+}
