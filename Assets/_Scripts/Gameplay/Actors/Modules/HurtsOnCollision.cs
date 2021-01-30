a28bf6e8f5cfea9145185007d3b1131662f9ade1
Szymon Maj <fishbaitharry@gmail.com>
2021-01-30 13:57:44 +0100

    fix Spikes

--- Assets/_Scripts/Gameplay/Actors/Modules/HurtsOnCollision.cs
+++ Assets/_Scripts/Gameplay/Actors/Modules/HurtsOnCollision.cs
@@ -20,13 +20,28 @@
 
         private void OnCollisionEnter2D(Collision2D collision)
         {
+            // This is for mines, who have solid bodies.
+            OnAnyKindOfContact(collision.collider);
+        }
+
+        private void OnTriggerEnter2D(Collider2D collider)
+        {
+            // This is for spikes, who you can pass through.
+            OnAnyKindOfContact(collider);
+        }
+
+        private void OnAnyKindOfContact(Collider2D collider)
+        {
+            var bodyObject = collider.attachedRigidbody.gameObject;
+            Debug.Log($"I collided with {bodyObject.name}.");
+
             // make sure it only hurts the player (who has Movement behavior)
-            if (collision.gameObject.GetComponent<Movement>() == null) { return; }
+            if (bodyObject.GetComponent<Movement>() == null) { return; }
 
             Debug.Log("Ouch! It hurt!");
             _Controller.Lifepoints -= _Damage;
 
-            var reaction = collision.gameObject.GetComponent<DamageReaction>();
+            var reaction = bodyObject.GetComponent<DamageReaction>();
             if (reaction != null) reaction.OnDamage();
         }
 
